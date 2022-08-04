95d96c6c5b7e016cb0f15c003211428d3a443fa8
Paulo Mariano <paulomariano2501@gmail.com>
2022-08-03 23:29:15 -0300

    fetch api working

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 
+type Repository = {
+  info: {
+    Sexo: string,
+    Idade: string
+  }
+  name: string,
+  images:string
+}
+
 function App() {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+useEffect(() =>{
+  fetch('https://naruto-api.herokuapp.com/api/v1/characters')
+  .then(response => response.json())
+  .then(data => {
+    setRepositories(data)
+  })
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      
-       <p>DASASDASDADS</p>
-      </header>
+      <div className='xx'>
+        aaaa
+      {repositories.map(repo => {
+        return(
+          <div key={repo.name} className="col">
+          <div className="card">
+              <div>
+                  <p></p>
+                  <strong className="name-strong">{repo.name}</strong>
+              </div>
+              <div className="status">{repo.info.Sexo} - {repo.info.Idade}</div>
+              <br />
+              <img src={repo.images[0]} alt={repo.images[0]} className="image" />
+              <br />
+              <br />
+              <br />
+
+              <div />
+          </div>
+      </div>
+
+)
+})}
+
+
+</div>
+
     </div>
   );
 }
